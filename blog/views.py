9675ae28c15640a8dc2234364dd9a195906ed4e3
Bharat Ahuja <bharatahuja198@gmail.com>
2020-09-05 03:40:55 +0530

    checked if user is authenticated before accessing pages

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,10 @@ from django.contrib import messages
 
 # Create your views here.
 def main(request):
-    return render(request, 'mainpage.html', {})
+    if not request.user.is_authenticated:
+        return render(request, 'mainpage.html', {})
+    else:
+        return redirect('/home')
 
 def post_list(request):
     if request.user.is_authenticated:
@@ -39,26 +42,32 @@ def about(request):
     if request.user.is_authenticated:
         return render(request, 'blog/about.html', {})
     else:
-        return redirect('/')
+        return redirect('/aboutme')
 
 def aboutme(request):
-    return render(request, 'aboutme.html', {})
+    if not request.user.is_authenticated:
+        return render(request, 'aboutme.html', {})
+    else:
+        return redirect('/about')
 
 def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f"New Account Created: {username}")
-            login(request, user)
-            return redirect("/home")
-        else:
-            for msg in form.error_messages:
-                messages.error(request, form.error_messages[msg])
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f"New Account Created: {username}")
+                login(request, user)
+                return redirect("/home")
+            else:
+                for msg in form.error_messages:
+                    messages.error(request, form.error_messages[msg])
 
-    form = UserCreationForm
-    return render(request, 'register.html', {'form':form})
+        form = UserCreationForm
+        return render(request, 'register.html', {'form':form})
+    else:
+        return redirect('/home')
 
 def logout_request(request):
     logout(request)
@@ -66,20 +75,23 @@ def logout_request(request):
     return redirect('/')
 
 def login_request(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.success(request, f"Successfully Logged In As {username}")
-                return redirect('/home')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, f"Successfully Logged In As {username}")
+                    return redirect('/home')
+                else:
+                    messages.error(request, "Invalid Username or Password")
             else:
-                messages.error(request, "Invalid Username or Password")
-        else:
-                messages.error(request, "Invalid Username or Password")
+                    messages.error(request, "Invalid Username or Password")
+    else:
+        return redirect('/home')
 
     form = AuthenticationForm()
     return render(request, "login.html", {"form": form})
