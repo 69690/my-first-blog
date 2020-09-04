95ab56977d51b1d5964cb8eb178a8cd611c55d71
Bharat Ahuja <bharatahuja198@gmail.com>
2020-09-05 03:10:52 +0530

    Successfully Completed Login and Register

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.main, name="main"),
+    path('home/', views.post_list, name='post_list'),
     # path('stories/story1', views.story1, name='story1'),
     # path('stories/story2', views.story2, name='story2'),
     # path('stories/story3', views.story3, name='story3'),
 
     path('story/<int:pk>/', views.post_detail, name='post_detail'),
     path('about', views.about, name='about'),
+    path('aboutme', views.aboutme, name='aboutme'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout_request, name='logout'),
+    path('login', views.login_request, name='login'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post 
 
 from django.shortcuts import render, get_object_or_404
 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
 # Create your views here.
+def main(request):
+    return render(request, 'mainpage.html', {})
+
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    if request.user.is_authenticated:
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        return render(request, 'blog/post_list.html', {'posts':posts})
+    else:
+        return redirect('/')
 
 # def story1(request):
 #     return render(request, 'blog/stories/story1.html', {})
@@ -19,8 +29,57 @@ def post_list(request):
 #     return render(request, 'blog/stories/story3.html', {})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
+    else:
+        return redirect('/')
 
 def about(request):
-    return render(request, 'blog/about.html', {})
+    if request.user.is_authenticated:
+        return render(request, 'blog/about.html', {})
+    else:
+        return redirect('/')
+
+def aboutme(request):
+    return render(request, 'aboutme.html', {})
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account Created: {username}")
+            login(request, user)
+            return redirect("/home")
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+
+    form = UserCreationForm
+    return render(request, 'register.html', {'form':form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged Out Successfully")
+    return redirect('/')
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Successfully Logged In As {username}")
+                return redirect('/home')
+            else:
+                messages.error(request, "Invalid Username or Password")
+        else:
+                messages.error(request, "Invalid Username or Password")
+
+    form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
